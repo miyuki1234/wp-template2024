ef2933b98cd9f8846e71b0a00ab101b2ce58f584
冨永みゆき <miyuki.t.web@gmail.com>
2024-02-19 09:05:47 +0900

    プライバシーポリシー / ページ作成

--- page-privacypolicy.php
+++ page-privacypolicy.php
@@ -1,145 +1,79 @@
 <?php get_header(); ?>
 
-
 <main class="main-area">
-
-
 
   <!-- ーーートップビューーーー -->
   <div class="page-top-view bg-normal">
     <div class="page-top-view__inner">
 
       <div class="title__outer">
-        <h2 class="title_theme_l font-en ta-center show slide-bottom">
-          プライバシーポリシー
+        <h2 class="title_theme_l show slide-bottom">
+          PRIVACY POLICY<span>プライバシーポリシー</span>
         </h2><!-- .title_theme_l-ja -->
       </div><!-- .title__outer -->
 
-
-      <!-- ーーーパンくずリストーーー -->
-      <div class="breadcrumb-area">
-        <div class="breadcrumb-area__inner">
-          <div id="breadcrumb">
-            <?php custom_breadcrumb(); ?>
-          </div><!-- #breadcrumb -->
-        </div><!-- .breadcrumb-area__inner -->
-      </div><!-- .breadcrumb-area -->
     </div><!-- .page-top-view__inner -->
   </div><!-- .page-top-view -->
 
+  <!-- ーーーパンくずリストーーー -->
+  <div class="breadcrumb-area">
+    <div class="breadcrumb-area__inner">
+      <div id="breadcrumb">
+        <?php custom_breadcrumb(); ?>
+      </div><!-- #breadcrumb -->
+    </div><!-- .breadcrumb-area__inner -->
+  </div><!-- .breadcrumb-area -->
 
-
-<!-- ーーーコンテンツーーー -->
-<section class="page-section-01 page-section section-black">
+  <!-- ーーーコンテンツーーー -->
+  <section class="page-section-01 page-section section-black">
 
     <div class="page-section-01__inner page-section__inner last-section__inner">
 
+      <h3 class="title_theme_m ta-center">個人情報の取扱いについて</h3><!-- .title_theme_m -->
 
       <div class="contents-box">
-        <h3 class="title_theme_m">はじめに</h3><!-- .title_theme_m -->
+        <h4 class="title_theme_2s title_border">１. 収集する情報とその目的</h4><!-- .title_theme_2s -->
         <p class="text_theme_s">
-          インソールと痛みの治療院（以下、「当院」）は、サービスの提供にあたり、お客様の個人情報をお預かりしております。<br>
-          当院は個人情報を保護し、お客様に更なる信頼性と安心感をご提供できるように努めて参ります。<br>
-          当院は、個人情報に関する法令を遵守し、個人情報の適切な取り扱いを実現いたします。
+        当社のウェブサイトでは、お問い合わせフォームを通じて、以下の個人情報を収集することがあります。
+        </p><!-- .text_theme_s -->
+        <ul class="list-disc">
+          <li>お名前</li>
+          <li>メールアドレス</li>
+          <li>電話番号（任意）</li>
+          <li>お問い合わせ内容</li>
+        </ul><!-- .list-disc -->
+        <p class="text_theme_s">
+        これらの情報は、お問い合わせに対する回答や必要に応じたサービスの提供を目的として使用されます。
         </p><!-- .text_theme_s -->
       </div><!-- .contents-box -->
 
       <div class="contents-box">
-        <h3 class="title_theme_m">個人情報の取得について</h3><!-- .title_theme_m -->
-        <p class="text_theme_s">当院は、偽りその他不正の手段によらず適正に個人情報を取得致します。なお、次のような場合に必要な範囲で個人情報を収集する場合があります。</p><!-- .text_theme_s -->
-        <ul class="list-none">
-          <li>１．ご相談・お問い合わせ</li>
-          <li>２．ご予約</li>
-        </ul>
+        <h3 class="title_theme_2s title_border">２. Googleアナリティクスの使用</h3><!-- .title_theme_m -->
+        <p class="text_theme_s">当社ウェブサイトでは、サービス向上およびウェブサイトの利便性向上のため、Googleアナリティクスを利用しています。これにより、匿名のトラフィックデータが収集されますが、個人を特定するものではありません。<br>
+        Googleアナリティクスの詳細は「<a class="text_link" href="https://marketingplatform.google.com/about/analytics/terms/jp/">Googleアナリティクス利用規約</a>」をご覧ください。</p><!-- .text_theme_s -->
       </div><!-- .contents-box -->
 
       <div class="contents-box">
-        <h3 class="title_theme_m">個人情報の第三者への開示</h3><!-- .title_theme_m -->
-        <p class="text_theme_s">個人情報は適切に管理し、以下に該当する場合を除いて第三者に開示することはございません。</p><!-- .text_theme_s -->
-        <ul class="list-disc">
-          <li>本人のご了解がある場合</li>
-          <li>法令等への協力のため、開示が必要となる場合</li>
-        </ul>
+        <h3 class="title_theme_2s title_border">３. reCAPTCHAの使用</h3><!-- .title_theme_m -->
+        <p class="text_theme_s">当セキュリティ強化およびスパム対策のため、当社のウェブサイトはGoogleのreCAPTCHAサービスを利用しています。このプロセスでは、ユーザーの行動が分析されますが、これもまた個人を特定するものではありません。</p><!-- .text_theme_s -->
       </div><!-- .contents-box -->
 
       <div class="contents-box">
-        <h3 class="title_theme_m">個人情報の開示・訂正・追加・削除・利用停止</h3><!-- .title_theme_m -->
-        <p class="text_theme_s">当院は、個人情報を以下の利用目的の達成に必要な範囲内で、利用いたします。<br>
-        以下に定めのない目的で個人情報を利用する場合、あらかじめご本人の同意を得た上で行ないます。</p><!-- .text_theme_s -->
-        <ul class="list-disc">
-          <li>１．ご相談・お問い合わせに対するご回答のご連絡のため</li>
-          <li>２．ご予約の確認のご連絡のため</li>
-          <li>３．個人情報を特定しない統計情報に利用するため</li>
-        </ul>
+        <h3 class="title_theme_2s title_border">４．個人情報の共有と開示</h3><!-- .title_theme_m -->
+        <p class="text_theme_s">当社は、法的要求がある場合や当社の権利や財産を守る必要がある場合を除き、お客様の同意なく第三者に個人情報を共有または開示することはありません。</p><!-- .text_theme_s -->
       </div><!-- .contents-box -->
 
       <div class="contents-box">
-        <h3 class="title_theme_m">個人情報の委託について</h3><!-- .title_theme_m -->
-        <p class="text_theme_s">当院は、個人情報の取り扱いの全部または一部を第三者に委託する場合は、当該第三者について厳正な調査を行い、取り扱いを委託された個人情報の安全管理が図られるよう当該第三者に対する必要かつ適切な監督を行います。</p><!-- .text_theme_s -->
+        <h3 class="title_theme_2s title_border">５．プライバシーポリシーの変更</h3><!-- .title_theme_m -->
+        <p class="text_theme_s">当社のプライバシーポリシーに関してご質問がある場合は、<a href="<?php echo home_url('/contact'); ?>">お問い合わせページ</a>ご連絡ください。</p><!-- .text_theme_s -->
       </div><!-- .contents-box -->
-
-      <div class="contents-box">
-        <h3 class="title_theme_m">個人情報の第三者提供について</h3><!-- .title_theme_m -->
-        <p class="text_theme_s">当院は、個人情報保護法等の法令に定めのある場合を除き、個人情報をあらかじめご本人の同意を得ることなく、第三者に提供いたしません。</p><!-- .text_theme_s -->
-      </div><!-- .contents-box -->
-
-      <div class="contents-box">
-        <h3 class="title_theme_m">個人情報の開示・訂正等について</h3><!-- .title_theme_m -->
-        <p class="text_theme_s">当院は、ご本人から自己の個人情報についての開示の請求がある場合、速やかに開示をいたします。<br>
-        その際、ご本人であることが確認できない場合には、開示に応じません。<br>
-        個人情報の内容に誤りがあり、ご本人から訂正・追加・削除の請求がある場合、調査の上、速やかにこれらの請求に対応いたします。
-        その際、ご本人であることが確認できない場合には、これらの請求に応じません。<br>
-        当院の個人情報の取り扱いにつきまして、上記の請求・お問い合わせ等ございましたら、下記までご連絡くださいますようお願い申し上げます。</p><!-- .text_theme_s -->
-        <div class="info-area">
-          <p class="title_theme_s">連絡先</p><!-- .title_theme_s -->
-          <dl>
-            <dt>名称</dt>
-            <dd>インソールと痛みの治療院</dd>
-            <dt>連絡先</dt>
-            <dd><a href="insoletoitami@gmail.com" target="_brank">insoletoitami@gmail.com</a></dd>
-          </dl>
-        </div><!-- .info-area -->
-      </div><!-- .contents-box -->
-
-      <div class="contents-box">
-        <h3 class="title_theme_m">アクセス解析ツールについて</h3><!-- .title_theme_m -->
-        <p class="text_theme_s">当サイトでは、Google Inc.が提供するアクセス解析ツール「Googleアナリティクス」を利用しています。 Googleアナリティクスは、トラフィックデータの収集のためにCookieを使用しています。<br>このトラフィックデータは匿名で収集されており、個人を特定するものではありません。この機能はCookieを無効にすることで収集を拒否することが出来ます。<br>
-          Googleアナリティクスの詳細は「<a href="https://marketingplatform.google.com/about/analytics/terms/jp/">Googleアナリティクス利用規約</a>」をご覧ください。</p><!-- .text_theme_s -->
-        </div><!-- .contents-box -->
-
-      <div class="contents-box">
-        <h3 class="title_theme_m">本方針の変更</h3><!-- .title_theme_m -->
-        <p class="text_theme_s">当店は、個人情報の管理方法を見直し、継続的に改善を実施します。</p><!-- .text_theme_s -->
-      </div><!-- .contents-box -->
-
-      <div class="contents-box">
-        <h3 class="title_theme_m">個人情報の管理方法の継続的改善について</h3><!-- .title_theme_m -->
-        <p class="text_theme_s">本方針の内容は変更されることがあります。<br>
-        変更後の本方針については、当店が別途定める場合を除いて、当サイトに掲載した時から効力を生じるものとします。</p><!-- .text_theme_s -->
-      </div><!-- .contents-box -->
-
-      <div class="contents-box">
-        <p class="text_theme_s ta-right">2023.06.01</p><!-- .text_theme_s -->
-      </div><!-- .contents-box -->
-
-
 
       </div><!-- .section-01__inner -->
-</section><!-- .section-01 -->
+  </section><!-- .section-01 -->
 
-  <?php get_template_part('blog-banner'); ?>
-
-<?php get_template_part('contact-banner'); ?>
-
-<?php get_template_part('contact-area'); ?>
-
+  <?php get_template_part('contact-banner'); ?>
 
 
 </main><!-- .main-area-->
 
-
-
-
-
-
-      <?php get_footer(); ?>
+<?php get_footer(); ?>
